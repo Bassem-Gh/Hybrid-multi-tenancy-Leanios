2f73cfba0f06108c3a8ae981906e5e660581a9aa
Bassem GH <bessem.gharsellaoui@gmail.com>
2023-09-21 20:41:16 +0100

    add db config func

--- app/models/company.rb
+++ app/models/company.rb
@@ -19,4 +19,81 @@ class Company < ApplicationRecord
     # Handle the exception here
     puts "An error occurred: #{e.message}"
   end
+
+  def fetch_all_db_configurations
+    [
+      {
+        'name' => 'tenant1',
+        'db_host' => 'localhost',
+        'db_port' => 5432,
+        'db_name' => 'primary',
+        'db_user' => 'postgres',
+        'db_password' => 'postgres'
+      },
+      {
+        'name' => 'tenant2',
+        'db_host' => 'localhost',
+        'db_port' => 5432,
+        'db_name' => 'secondary_database',
+        'db_user' => 'postgres',
+        'db_password' => 'postgres'
+      },
+      {
+        'name' => 'tenant3',
+        'db_host' => 'localhost',
+        'db_port' => 5432,
+        'db_name' => 'secondary_database',
+        'db_user' => 'postgres',
+        'db_password' => 'postgres'
+      },
+      {
+        'name' => 'mercedes',
+        'db_host' => 'localhost',
+        'db_port' => 5432,
+        'db_name' => 'third_database',
+        'db_user' => 'postgres',
+        'db_password' => 'postgres'
+      },
+      {
+        'name' => 'bmw',
+        'db_host' => 'localhost',
+        'db_port' => 5432,
+        'db_name' => 'third_database',
+        'db_user' => 'postgres',
+        'db_password' => 'postgres'
+      },
+      {
+        'name' => 'toyota',
+        'db_host' => 'localhost',
+        'db_port' => 5432,
+        'db_name' => 'third_database',
+        'db_user' => 'postgres',
+        'db_password' => 'postgres'
+      },
+      'name' => 'tesla',
+      'db_host' => 'localhost',
+      'db_port' => 5432,
+      'db_name' => 'third_database',
+      'db_user' => 'postgres',
+      'db_password' => 'postgres'
+    ]
+  end
+
+  def db_configuration(tenant_name)
+    configurations = fetch_all_db_configurations
+    config = configurations.find { |c| c['name'] == tenant_name }
+    if config
+      {
+        adapter: 'postgresql',
+        host: config['db_host'],
+        port: config['db_port'],
+        database: config['db_name'],
+        username: config['db_user'],
+        password: config['db_password']
+      }
+    else
+      # Handle configuration not found
+      {}
+    end
+  end
 end
