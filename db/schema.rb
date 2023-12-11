c13471bf3d233ee433a62a1f62502dbf4c6566c5
Bassem GH <bessem.gharsellaoui@gmail.com>
2023-12-11 11:01:37 +0100

    remove yarn.lock file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_12_150549) do
+ActiveRecord::Schema.define(version: 2023_11_27_164052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 2023_10_12_150549) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "database"
     t.json "database_config"
+  end
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "projects", force: :cascade do |t|
