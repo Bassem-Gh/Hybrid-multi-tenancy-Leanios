e8e8124f27341acad18ab6eef26c12cbf2151fa8
Bassem GH <bessem.gharsellaoui@gmail.com>
2024-01-11 10:04:33 +0100

    switch monkey patch

--- config/initializers/apartment_monky_patch.rb
+++ config/initializers/apartment_monky_patch.rb
@@ -2,10 +2,22 @@
 module Apartment
   module Tenant
     def self.switch!(tenant)
+      return if tenant == 'public'
+
       # establish connection with the tenant database
       tenant_ob = Company.find_by(subdomain: tenant)
       ActiveRecord::Base.establish_connection(tenant_ob.database_config)
       super
+    end
+
+    def self.switch(tenant, &block)
+      return if tenant == 'public'
+
+      # establish connection with the tenant database
+      tenant_ob = Company.find_by(subdomain: tenant)
+      ActiveRecord::Base.establish_connection(tenant_ob.database_config)
+
+      super(tenant, &block)
     end
   end
 end
